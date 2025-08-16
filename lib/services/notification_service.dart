1e41cb7c2adeb0af1e9044b0c5a9c7f84fe08f4a
abd33 <alaziizz67@gmail.com>
2025-08-16 14:03:32 +0300

    notification feature added

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,4 +1,3 @@
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path/path.dart';
